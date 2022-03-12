2c9b8e27db6811c552e2204c46efdaf058593889
Claiton Nazaret <claitonnazaret@gmail.com>
2022-03-12 15:01:43 -0300

    initial project structure

---
+++ react-auth-hooks/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginPage from './pages/login.page';
+import DashboardPage from './pages/dashboard.page';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+        <DashboardPage />
+    </div>
+  );
+}
+
+export default App;
